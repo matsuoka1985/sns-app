87f03d3ba8dcc07b968b2fbe7091665fbd044551
matsuoka1985 <matsuoka1985@yahoo.co.jp>
2025-08-26 06:13:53 +0900

    Add production Docker build and deploy workflow3

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,8 +1,9 @@
 # SSM Parameters for sensitive data
 resource "aws_ssm_parameter" "app_key" {
-  name  = "/${var.project_name}/app_key"
-  type  = "SecureString"
-  value = var.app_key
+  name      = "/${var.project_name}/app_key"
+  type      = "SecureString"
+  value     = var.app_key
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-app-key"
@@ -10,9 +11,10 @@ resource "aws_ssm_parameter" "app_key" {
 }
 
 resource "aws_ssm_parameter" "db_password" {
-  name  = "/${var.project_name}/db_password"
-  type  = "SecureString"
-  value = var.db_password
+  name      = "/${var.project_name}/db_password"
+  type      = "SecureString"
+  value     = var.db_password
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-db-password"
@@ -20,9 +22,10 @@ resource "aws_ssm_parameter" "db_password" {
 }
 
 resource "aws_ssm_parameter" "firebase_credentials" {
-  name  = "/${var.project_name}/firebase_credentials"
-  type  = "SecureString"
-  value = var.firebase_credentials
+  name      = "/${var.project_name}/firebase_credentials"
+  type      = "SecureString"
+  value     = var.firebase_credentials
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-firebase-credentials"
