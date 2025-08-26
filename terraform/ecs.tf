39c38d2ea9d93d996d6c15f2b959ec3203250379
matsuoka1985 <matsuoka1985@yahoo.co.jp>
2025-08-26 21:48:15 +0900

    Add Route53 record cleanup to prevent conflicts

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -195,7 +195,7 @@ resource "aws_ecs_service" "app" {
 
   load_balancer {
     target_group_arn = aws_lb_target_group.app.arn
-    container_name   = "laravel-app"
+    container_name   = "nginx"
     container_port   = 80
   }
 
